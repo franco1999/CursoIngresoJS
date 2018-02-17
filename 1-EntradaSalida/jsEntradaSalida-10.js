6e84e385488b54287b10cdf3d5a3486a529324f4
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-02-16 22:03:33 -0300

    resueltos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,23 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultado; 
+	var descuento;
+
+	importe = document.getElementById('importe').value;
+
+	importe = parseInt(importe);
+	resultado = parseInt (resultado);
+
+	descuento = importe * 25 / 100;
+
+    resultado = importe - descuento;
+
+    document.getElementById('resultado').value = resultado;
+
+    /* no olvidar los puntos y coma, para hacer funcionar
+     la barra de resultado no olvidar el tomar por id al 
+     final la barra de resultados
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,24 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	var aumento;  
+
+	sueldo = document.getElementById('sueldo').value;
+
+	sueldo = parseInt(sueldo);
+	resultado = parseInt(resultado);
+
+	aumento = sueldo * 10 / 100;
+
+	resultado = sueldo + aumento;
+
+	document.getElementById('resultado').value = resultado; 
+
+
+
+
 
 	
 }
