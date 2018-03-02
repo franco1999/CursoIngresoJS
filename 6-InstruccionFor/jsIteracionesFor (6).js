9a6361e7a8d51c4a738d13e2b1d5aa98df0a37f7
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-03-02 19:55:14 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,5 +1,20 @@
 function Mostrar()
 {
+	var numero
+	var contador=0; 
+	
+
+	numero = prompt("ingrese un numero");
+	numero = parseInt(numero);
+
+	for(var i=2 ; i<numero ; i++){ 
+
+		if(i % 2==0) {
+			contador++
+			console.log(i);
+		}
+	
+	}
 
 
 
