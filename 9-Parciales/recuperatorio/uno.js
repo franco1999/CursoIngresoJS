ac913b514760f2ad945eda6b193e29a6f66d8b0b
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-07-05 22:17:18 -0300

    05/07/18
    Clase dos

--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,18 @@
 
 function Mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base=prompt("Ingrese la base");
+	altura=prompt("Ingrese la altura");
+
+	superficie=base*altura;
+	perimetro=base+altura;
+
+	alert("La superficie es "+superficie+" y el perimetro es "+perimetro);
+
 
 }
