780e58942ca3735bfa7df4796a0a3f9c91c18808
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-07-12 22:07:12 -0300

    12/07/2018
    Clase tres

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,68 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno;
+	var percioDos;
+	var precioTres;
+	var resultadoFinal;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+
+	resultadoFinal=precioUno+precioDos+precioTres;
+
+	alert("El resultado de la suma es "+resultadoFinal);
+
+}
+	
+	function Promedio () 
+{
+	var precioUno;
+	var percioDos;
+	var precioTres;
+	var resultadoFinal;
+	var resultadoPromedio;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+
+	resultadoFinal=precioUno+precioDos+precioTres;
+	resultadoPromedio=resultadoFinal/3;
+
+
+	alert("El promedio es "+resultadoPromedio);
 	
 }
-function Promedio () 
-{
 	
-}
-function PrecioFinal () 
+	function PrecioFinal () 
 {
+	var precioUno;
+	var percioDos;
+	var precioTres;
+	var resultadoFinal;
+	var precioIva;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+
+	resultadoFinal=precioUno+precioDos+precioTres;
+	precioIva=121*resultadoFinal/100;
+
+	alert("El precio final es de "+precioIva); 
 	
 }
