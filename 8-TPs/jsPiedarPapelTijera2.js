b44445532bd8f058c981f2f4de5e0f7feea6474d
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-07-23 22:04:14 -0300

    24/07/18

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -6,6 +6,9 @@ var ContadorDePerdidas=0;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
+	eleccionMaquina=Math.floor(Math.random()*3)+1;
+
+
 	 	
 
 
@@ -13,7 +16,27 @@ function comenzar()
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
+	var piedra=1;
+
+	if(eleccionMaquina==piedra){
+		ContadorDeEmpates>+1;
+		alert("EMPATÒ");
 	
+	} else if(eleccionMaquina==3) {
+		ContadorDeGanadas+1;
+		alert("GANÒ");
+	
+	} else {
+		ContadorDePerdidas++;
+		alert("PERDIÒ");
+
+	} 
+
+	// acumulador= var 1 + var 2
+	
+	}
+
+	mostarResultado
 
 }//FIN DE LA FUNCIÓN
 function papel()
@@ -27,5 +50,8 @@ function tijera()
 
 function mostarResultado()
 {
+	document.getElementById('ganadas').value=ContadorDeGanadas;
+	document.getElementById('perdidas').value=ContadorDePerdidas;
+	document.getElementById('empatadas').value=ContadorDeEmpates;
 
 }
