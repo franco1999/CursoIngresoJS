9b5f93b748796e2fb8124d5cf68c64da7c6baec6
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-07-30 21:48:13 -0300

    Ejercicio en clase
    30/07/2018

---
+++ 9-Parciales/siete.js
@@ -0,0 +1,37 @@
+/* NOMBRE|SEXO|EDAD	
+
+   JOSE    M    25 
+   
+   MARIA   F    16
+   
+   JESUS   M    33
+   
+   FER     M    81*/
+
+function mostrar()
+{	
+	var sexo;
+	var edad;
+	var contador=0;
+	var nombre;
+
+	contador=0;
+	
+	while(contador<4)
+	{
+		contador++;
+		nombre=prompt("Ingrese nombre valido");
+		
+		sexo=prompt("Ingrese sexo valido");
+		while(sexo!="m"&&sexo"f")
+		{
+			sexo=prompt("Ingrese sexo valido");
+		}
+		edad=prompt("Ingrese edad");
+		edad=parseInt(edad);
+		while(insNaN(edad)||edad<0||edad>100)
+		{
+			edad=prompt("Ingrese edad");
+			edad=parseInt(edad);
+		}
+}
