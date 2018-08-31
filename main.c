482fa7f4eee3df5a0e2676cebf9a05e51011973d
franco1999 <36521789+franco1999@users.noreply.github.com>
2018-08-31 10:28:42 -0300

    31/08

--- main.c
+++ main.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int pedirEntero(char[]);
+
 int main()
-/*{
-    printf("Hello world!\n");
-    return 0;
-}*/
 {
-    int nro1,nro2,nro3;
-    printf("Ingrese: ");
-    scanf("%d", &nro1);
-    printf("\n Ingrese: ");
-    scanf( "%d", &nro2);
-    printf("\n Ingrese: ");
-    scanf("%d", &nro3);
+    int edad;
+    edad= pedirEntero("Ingrese su edad: ");
+    return 0;
+}
+//puntero = es una direccion de variable que guarda otra variable
 
-    if(nro1>nro2 && nro1>nro3){
-        printf("El numero mayor es %d", nro1);
-    } else {
-        if(nro2>nro1 && nro2>nro3){
-            printf("El numero mayor es %d", nro2);
-        } else {
-            printf("El numero mayor es %d", nro3);
+int pedirEntero(char texto[], int min, int max)
+{
+    int numero;
+    printf("%s", texto);
+    while(numero<min || numero>max)
+        {
+            int numero;
+            printf("%s", texto);
         }
-    }
-
 }
