80ba53612c1014beca55a2e34142a6df395f1421
franco1999 <36521789+franco1999@users.noreply.github.com>
2019-03-29 12:13:42 -0300

    programacion 1
    29/03

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pedirEntero(char[]);
-
 int main()
 {
-    int edad;
-    edad= pedirEntero("Ingrese su edad: ");
-    return 0;
-}
-//puntero = es una direccion de variable que guarda otra variable
+    int x;
+    int y;
+    int suma;
+    int resta;
+    int division;
+    int multiplicacion;
+    int factorial;
 
-int pedirEntero(char texto[], int min, int max)
-{
-    int numero;
-    printf("%s", texto);
-    while(numero<min || numero>max)
-        {
-            int numero;
-            printf("%s", texto);
-        }
+
+
+
+    scanf("%d",suma);
+
+    return 0;
+
+
 }
